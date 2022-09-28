9ebdbe6ea8c0927b765cd1203714ecf0219ac488
alex <aok@buran.rest>
2022-09-28 14:59:21 +0300

    background control works

--- src/main.cpp
+++ src/main.cpp
@@ -5,31 +5,75 @@
 #include <imgui-SFML.h>
 #include <imgui.h>
 
+
+// цвет фона
+static sf::Color bgColor;
+// значение цвета по умолчанию
+float color[3] = {0.12f, 0.12f, 0.13f};
+
+// задать цвет фона по вещественному массиву компонент
+static void setColor(float * pDouble){
+    bgColor.r = static_cast<sf::Uint8>(pDouble[0] * 255.f);
+    bgColor.g = static_cast<sf::Uint8>(pDouble[1] * 255.f);
+    bgColor.b = static_cast<sf::Uint8>(pDouble[2] * 255.f);
+}
+
+// главный метод
 int main() {
-    sf::RenderWindow window(sf::VideoMode(1280, 720), "ImGui + SFML = <3");
+    // создаём окно для рисования
+    sf::RenderWindow window(sf::VideoMode(1280, 720), "Geometry Project 10");
+    // задаём частоту перерисовки окна
     window.setFramerateLimit(60);
+    // инициализация imgui+sfml
     ImGui::SFML::Init(window);
 
+    // задаём цвет фона
+    setColor(color);
+
+    // переменная таймера
     sf::Clock deltaClock;
+    // пока окно открыто, запускаем бесконечный цикл
     while (window.isOpen()) {
+        // создаём событие sfml
         sf::Event event;
+        // пока окно принимает события
         while (window.pollEvent(event)) {
+            // отправляем события на обработку sfml
             ImGui::SFML::ProcessEvent(event);
 
+            // если событие - это закрытие окна
             if (event.type == sf::Event::Closed) {
+                // закрываем окно
                 window.close();
             }
         }
 
+        // запускаем обновление окна по таймеру с заданной частотой
         ImGui::SFML::Update(window, deltaClock.restart());
 
-        ImGui::ShowDemoWindow();
 
-        window.clear();
+        // создаём окно управления
+        ImGui::Begin("Control");
+
+        // Инструмент выбора цвета
+        if (ImGui::ColorEdit3("Background color", color)) {
+            // код вызывается при изменении значения
+            // задаём цвет фона
+            setColor(color);
+        }
+        // конец рисование окна
+        ImGui::End();
+
+
+        // очищаем окно
+        window.clear(bgColor);
+        // рисуем по окну средствами imgui+sfml
         ImGui::SFML::Render(window);
+        // отображаем изменения на окне
         window.display();
     }
 
+    // завершаем работу imgui+sfml
     ImGui::SFML::Shutdown();
 
     return 0;
